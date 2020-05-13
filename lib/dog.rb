4655fee5f66b03b01e8b7f9c928aa44abe347e6d
kiku ishimoto <kikuish@gmail.com>
2020-05-13 00:31:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
-# Add your code here
+class Dog
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+
+  def all 
+    @@all
+  end 
+
+  def print_all 
+    @@all.each do |name|
+      puts dog.name 
+    end 
+  end 
+    
+end 
